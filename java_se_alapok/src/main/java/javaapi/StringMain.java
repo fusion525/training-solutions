2a497e7670473fd265be5f7b0567997397a35024
fusion525 <fusion525@users.noreply.github.com>
2020-11-01 16:24:19 +0100

    API exercise has been added

---
+++ java_se_alapok/src/main/java/javaapi/StringMain.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class StringMain {
+    public static void main(String[] args) {
+        String string = "Árvíztűrő tükör fúrógép";
+
+        System.out.println(string.toUpperCase());
+    }
+}

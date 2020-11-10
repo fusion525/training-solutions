17cdb56d90fefa04f0223ffc6809fe87dd557b6d
fusion525 <fusion525@users.noreply.github.com>
2020-11-10 13:49:00 +0100

    week03 11.10

---
+++ src/main/java/week03/Position.java
@@ -0,0 +1,34 @@
+package week03;
+
+import java.util.ArrayList;
+
+public class Position {
+
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Position> arrayList = new ArrayList<>();
+        arrayList.add(new Position("Boss", 150_000) );
+        arrayList.add(new Position("Manager", 100_000) );
+        arrayList.add(new Position("Programmer", 1_500_000) );
+
+        for (Position positions : arrayList) {
+            if (positions.getBonus() > 150_000) {
+                System.out.println(positions);
+            }
+        }
+    }
+
+}
+
+
